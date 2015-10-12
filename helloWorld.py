0e8c93c0809f170dda207fd2c0ef2645af29d145
Lars Hoeger <lars@NB-LH>
2015-10-12 22:55:33 +0200

    Kompletter Umbau auf World Klasse

--- helloWorld.py
+++ helloWorld.py
@@ -14,82 +14,117 @@ import threading
 import Queue
 import time
 
-class bubble(threading.Thread):
-    
-    def __init__(self, INTx, INTy, IMGbg, SCRscreen):
-        threading.Thread.__init__(self)
-        self.__STRfilenames__ = ['bubble1.png', 'bubble2.png', 'bubble3.png']
-        self.__IMGbubble__ = pygame.image.load(self.__STRfilenames__[random.randint(0,len(self.__STRfilenames__)-1)]).convert_alpha()
-        self.__h__ = self.__IMGbubble__.get_height()        
-        self.__x__ = INTx
-        self.__y__ = INTy
-        self.__bg__ = IMGbg
-        self.__screen__ = SCRscreen
-        self.start()
-    
-    def run(self):
-        while self.__y__ > self.__h__ * -1:
-            #screen.blit(self.__bg__,(0,0))
-            self.__screen__.blit(self.__IMGbubble__, (self.__x__, self.__y__))
-            pygame.display.update()
-            self.__y__ -= 1
-            time.sleep(0.01)
-            
 
 
-class ScreenUpdater(threading.Thread):
+class CLSworld(object):
     
     def __init__(self):
-        threading.Thread.__init__(self)
-        self.BOOloop = True
-        self.start()
+        pygame.init()
+        self.__STRfilename__ = "bg.png"
+        self.__LSTobjects__ = []
+        self.__TPLresolution__ = (800, 480)
+        self.Screen = pygame.display.set_mode(self.__TPLresolution__, 0, 32)
+        self.__IMGbgimage__ = pygame.image.load(self.__STRfilename__).convert_alpha()
         
-    def run(self):
-        while BOOloop:
-            pygame.display.update()
-            time.sleep(0.005)
+    
+    def process(self):
+        if len(self.__LSTobjects__) > 0:        
+            for OBJtmp in self.__LSTobjects__:
+                OBJtmp.process()
+
+    
+    def render(self):
+        self.Screen.blit(self.__IMGbgimage__,(0,0))
+        if len(self.__LSTobjects__) > 0:        
+            for OBJtmp in self.__LSTobjects__:
+                OBJtmp.render()
+        pygame.display.update()
+        
+    
+    def AddObject(self, OBJobject):
+        self.__LSTobjects__.append(OBJobject)
+
+
+
+class bubble():
+    
+    def __init__(self, OBJworld, INTx, INTy):
+        self.__STRfilenames__ = ['bubble1.png', 'bubble2.png', 'bubble3.png']
+        self.__IMGimage__ = pygame.image.load(self.__STRfilenames__[random.randint(0,len(self.__STRfilenames__)-1)]).convert_alpha()
+        self.__h__ = self.__IMGimage__.get_height()        
+        self.__x__ = INTx
+        self.__y__ = INTy
+        self.__world__ = OBJworld
+        OBJworld.AddObject(self)
         
         
+    def process(self):        
+        
+        if self.__y__ > -self.__h__:
+            INTxmovement = -1 + random.randint(0,2)
+            INTymovement = random.randint(1,4)
+            self.__x__ += INTxmovement
+            self.__y__ -= INTymovement
+        
+    
+    def render(self):
+        self.__world__.Screen.blit(self.__IMGimage__, (self.__x__, self.__y__))
+            
+            
 
-STRbg = 'bg.png'
-STRfish = 'fish.png'
+class fish():
+    
+    def __init__(self, OBJworld, INTx, INTy):
+        self.__STRfilename__ = "fish.png"
+        self.__IMGimage__ = pygame.image.load(self.__STRfilename__).convert_alpha()
+        self.__h__ = self.__IMGimage__.get_height()        
+        self.__x__ = INTx
+        self.__y__ = INTy
+        self.__world__ = OBJworld
+        OBJworld.AddObject(self)
+        
+        
+    def process(self):
+        self.__x__, self.__y__ = pygame.mouse.get_pos()
+        self.__x__ -= self.__IMGimage__.get_width() / 2
+        self.__y__ -= self.__IMGimage__.get_height() / 2
+        
+        
+        
+    def render(self):
+        self.__world__.Screen.blit(self.__IMGimage__, (self.__x__, self.__y__))
 
-pygame.init()
 
-screen = pygame.display.set_mode((640, 480), 0, 32)
-pygame.display.set_caption("Hallo Oscar")
-IMGbg = pygame.image.load(STRbg).convert()
-IMGfg = pygame.image.load(STRfish).convert_alpha()
 
-BOOfullscreen = False
+#pygame.display.set_caption("Hallo Oscar")
+#BOOfullscreen = False
+Game = CLSworld()
+Pointer = fish(Game, 0, 0)
 
-#SU = ScreenUpdater()
 clock = pygame.time.Clock()
+
 while True:
-    #time.sleep(0.01)
-    clock.tick(30)
+    
+    clock.tick(60)
+    
     for event in pygame.event.get():
-        if event.type == QUIT:
-            #SU.BOOloop = False
+        if event.type == QUIT:            
             exit()
-        elif event.type == KEYDOWN:
-            if event.key == K_f:
-                BOOfullscreen = not BOOfullscreen
-                if BOOfullscreen:
-                    screen=pygame.display.set_mode((640, 480), FULLSCREEN, 32)
-                else:
-                    screen=pygame.display.set_mode((640, 480), 0, 32)
+
+#        elif event.type == KEYDOWN:
+#            if event.key == K_f:
+#                BOOfullscreen = not BOOfullscreen
+#                if BOOfullscreen:
+#                    screen=pygame.display.set_mode((640, 480), FULLSCREEN, 32)
+#                else:
+#                    screen=pygame.display.set_mode((640, 480), 0, 32)
+
         elif event.type == MOUSEBUTTONDOWN:
-            #px = pygame.PixelArray(screen)
             INTx, INTy = pygame.mouse.get_pos()
-            b = bubble(INTx, INTy, IMGbg, screen)
+            b = bubble(Game,INTx, INTy)
             #print "%s:%s -> %s" % (INTx,INTy,screen.get_at((INTx, INTy)))
             #px=None
         
-    screen.blit(IMGbg,(0,0))
-    INTx, INTy = pygame.mouse.get_pos()
-    INTx -= IMGfg.get_width() / 2
-    INTy -= IMGfg.get_height() / 2
-    screen.blit(IMGfg, (INTx, INTy))
-    pygame.display.update()
+    Game.process()
+    Game.render()    
  
